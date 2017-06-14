f14069cff9bae7ab0f845ede93814e7e5635d49d
RODRIGO BUHLER <rbuhler@users.noreply.github.com>
2017-06-14 17:41:48 -0300

    Add files via upload

---
+++ windows/elastic.bat
@@ -0,0 +1,10 @@
+
+SET elastic_home=\Users\i820244\DevTools\elasticsearch-5.4.0\bin\
+SET kibana_home=\Users\i820244\DevTools\kibana-5.4.0-windows-x86\bin\
+SET logstach_home=\Users\i820244\DevTools\logstash-5.4.0\bin\
+
+SET elastic_exec=%elastic_home%elasticsearch.bat
+SET kibana_exec=%kibana_home%kibana.bat
+SET logstasg_exec=%logstash_home%logstash.bat
+
+%elastic_exec%
---
+++ windows/kibana.bat
@@ -0,0 +1,7 @@
+SET kibana_home=\Users\i820244\DevTools\kibana-5.4.0-windows-x86\bin\
+SET kibana_exec=%kibana_home%kibana.bat
+
+cls
+echo Starting Kibana ...
+echo %kibana_exec%
+%kibana_exec%
